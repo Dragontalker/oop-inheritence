e04c2c8694a2b9a13836629f086fcd93ca0607fa
Richard <richard.yang.tong@gmail.com>
2021-04-30 17:26:45 -0400

    Added state components.

---
+++ src/academy/learnprogramming/Car.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+}
