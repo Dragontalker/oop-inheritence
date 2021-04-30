fe6449357b067865418e0e5f0177dda5c23b9eb9
Richard <richard.yang.tong@gmail.com>
2021-04-30 19:57:31 -0400

    Inspected the default null value of state,

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
         Car porsche = new Car();
         Car holden = new Car();
-        porsche.setModel("Carrera");
+        System.out.println("Model is " + porsche.getModel());
+        porsche.setModel("911");
+        System.out.println("Model is " + porsche.getModel());
     }
 }
