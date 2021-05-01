49a55fb1866ee9a79125df4ae021497ed5a298ca
Richard <richard.yang.tong@gmail.com>
2021-04-30 20:06:31 -0400

    Added a getter method.

--- src/academy/learnprogramming/SimpleCalculator.java
+++ src/academy/learnprogramming/SimpleCalculator.java
@@ -3,4 +3,8 @@ package academy.learnprogramming;
 public class SimpleCalculator {
     private double firstNumber;
     private double secondNumber;
+
+    public double getFirstNumber() {
+        return this.firstNumber;
+    }
 }
