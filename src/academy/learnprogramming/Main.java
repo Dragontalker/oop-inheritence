127f01afc59cbb4734b7e14d566cfc1fb9a7cfd6
Richard <richard.yang.tong@gmail.com>
2021-04-30 20:16:30 -0400

    Added tests.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -4,11 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Car porsche = new Car();
-        Car holden = new Car();
-        porsche.setModel("m3");
-        System.out.println("Model is " + porsche.getModel());
-        porsche.setModel("911");
-        System.out.println("Model is " + porsche.getModel());
+        // Test 1
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add = " + calculator.getAdditionResult());
+        System.out.println("subtract = " + calculator.getSubtractionResult());
+
+        // Test 2
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply = " + calculator.getMultiplicationResult());
+        System.out.println("divide = " + calculator.getDivisionResult());
     }
 }
