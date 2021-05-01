f55f8cd02de60a822a852ceb217816c3669143f9
Richard <richard.yang.tong@gmail.com>
2021-04-30 20:18:18 -0400

    Added getter method getFirstName.

--- src/academy/learnprogramming/Person.java
+++ src/academy/learnprogramming/Person.java
@@ -4,4 +4,8 @@ public class Person {
     private String firstName;
     private String lastName;
     private int age;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
 }
